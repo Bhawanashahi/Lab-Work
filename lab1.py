ed733ab9c1653f6f2c0bd6dfef795719130bb3e7
Bhawana shahi <shahivawana61@gmail.com>
2021-12-03 13:23:14 +0545

    gitlab

---
+++ lab1.py
@@ -0,0 +1,7 @@
+# sum of three numbers
+x= int(input("enter first number"))
+y= int(input("enter second number"))
+z= int(input("enter third number"))
+
+sum = x+y+z
+print("the sum of three number is {} .".format)
---
+++ lab4.py
@@ -0,0 +1,4 @@
+# sum of odd number of first 10 positive integer
+list =[1,3,5,7,9]
+sum0 = sum(listnumb)
+print(sum0)
